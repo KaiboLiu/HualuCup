d7c97ededeab584baec01918e098eb567855f76f
Liu Kaibo <liukaibo87@gmail.com>
2018-07-30 19:39:51 -0700

    add two versions of dynamic(with flow) and static(no flow)

--- js/demo_static.js
+++ js/demo_static.js
@@ -1,11 +1,14 @@
-var day = "day1";
-var weather = "weather1";
+var day = "weekends";
+var weather = "regular";
 var timeInput = 2;//8:00;
 var timeFormat = "";
-
+var SVGdisplayed = false;
+var line_name = ['161','608','656','660','686','688','845','849','870','962'];
+var y_title = 50;
 //var dataFile = "./data/"+day+"_"+weather; //"day1_weather1";
 //var dataFile = "https://raw.githubusercontent.com/KaiboLiu/HualuCup/master/data/day1_weather1.dat";
-var dataFile = "https://raw.githubusercontent.com/KaiboLiu/HualuCup/master/data/try.dat";
+var dataPath = "https://raw.githubusercontent.com/KaiboLiu/HualuCup/master/data/";
+var dataFile = "";
 
 
 function getNode(n, v) {
@@ -24,17 +27,19 @@ function DC_nextInput(){
     //confirmSeq();
     var videoDiv = document.getElementById('demoVideo');
     if (videoDiv != null) videoDiv.remove(); 
-    //DC_addTable(0,1);
+    if (SVGdisplayed) {
+        document.getElementById("inputShown").innerHTML = $("#dayid").find('option:selected').text() + "_" + $("#weatherid").find('option:selected').text();//.slice(7);
+        DC_init_map();
+    }
 }
 
 function DC_confirmInput(){
     weather = $("#weatherid").find('option:selected').val();
     document.getElementById("inputShown").innerHTML = $("#dayid").find('option:selected').text() + "_" + $("#weatherid").find('option:selected').text();//.slice(7);
     document.getElementById("activeDispDiv").style.display = "block";
-    logView1 = true;
-    logView2 = true;
+    SVGdisplayed = true;
     DC_init_map();
-    DC_removeTable();
+    
 }
 
 function DC_tempTime(){
@@ -55,9 +60,8 @@ function DC_tempTime(){
 function DC_change(hint) {
     document.getElementById("timeInput").innerHTML = timeFormat;
     document.getElementById("slidebarHint").innerHTML = hint;
-    console.log('time of day: ' + timeFormat);
+    //console.log('time of day: ' + timeFormat);
     DC_init_map();
-    //fillPage_go(d=40,R=250,circleScale=50,halfOpen=20);
 }
 
 
@@ -98,12 +102,13 @@ function DC_val2Color(index){   //convert to  0-100
 
 
 function DC_init_map(){
+    dataFile = dataPath + day +'_'+ weather+'.dat';
+    DC_removeTable();
     $.getJSON(dataFile, function(data, status) {
         console.log(status);
         $("#mySVG").empty();
         
         data = data.map[0];
-        console.log(''+data.n_line);  
         console.log(data);  
         DC_render_map(data.n_line, data.n_stops, data.names, data.coordinates,
                       data.index_up[timeInput],data.index_down[timeInput]);
@@ -117,9 +122,10 @@ function DC_render_map(n_line, n_stops, names, coordinates, index_up, index_down
     var svg = document.getElementById('mySVG');
     var color_dot_stroke = '#33CAFF';//'#4842FF';// dark blue'white';
     var color_dot_fill = 'white';//'#33CAFF';// light blue 'blue';
-    var dot_stoke_wid = 2;
-    var offset = 3;//7;
-    var sideWidth = 5;//5;
+    var dot_stoke_wid = 3;
+    var dot_radius = 8;
+    var offset = 5;//7;
+    var sideWidth = 4;//5;
     var centerWidth = offset*2 - sideWidth;
     var dashLength = 25;
     //check if current explorer support Canvas object, to avoid sytax error in some html5-unfriendly explorers.
@@ -129,10 +135,22 @@ function DC_render_map(n_line, n_stops, names, coordinates, index_up, index_down
 
         var p1 = new Object;
         var p2 = new Object;
+        var legend;
+        var legendSize = 15;// font size in pixel
+        var legendWid = 5;  // number of char per line
+
+
         for (var lineNo=0; lineNo<n_line; lineNo++){
         //for (var lineNo=8; lineNo<9; lineNo++){
+
             p1.x = coordinates[lineNo][0];
             p1.y = coordinates[lineNo][1];
+
+            // add title(line name) for each line in map
+            legend = getNode('text', {x: p1.x-5, y:y_title/2, fontFamily:"Courier", fontSize:25, fill:'#33FFA5'});
+            legend.innerHTML = line_name[lineNo]+'路';
+            svg.appendChild(legend);            
+
             // draw n-1 segments
             for (var stopNo=1; stopNo<n_stops[lineNo]; stopNo++){
                 p2.x = coordinates[lineNo][stopNo*2];
@@ -141,23 +159,33 @@ function DC_render_map(n_line, n_stops, names, coordinates, index_up, index_down
                 var alpha = Math.atan((p2.y-p1.y)/(p2.x-p1.x));
                 var dx = offset * Math.sin(alpha), dy = offset * Math.cos(alpha);
                 if ((alpha * (p2.y-p1.y) < 0) || ((p2.y==p1.y)&&(p2.x<p1.x))){dx = 0-dx; dy = 0-dy}
-                //console.log(dx+','+dy+'');
+                
                 var traffic_color_up    = DC_val2Color(index_up[lineNo][stopNo-1]);
                 var traffic_color_down  = DC_val2Color(index_down[lineNo][stopNo-1]);
-                console.log(lineNo+'_'+stopNo+':'+traffic_color_up+'/'+traffic_color_down);
-                var newline_up   = getNode('line', {x1: p1.x-dx, y1:p1.y+dy, x2:p2.x-dx, y2:p2.y+dy, 
-                                                    stroke:traffic_color_up,   strokeWidth:sideWidth, strokeDasharray:dashLength+" "+dashLength, strokeDashoffset:dashLength*2,
-                                                    class:"tooltips-seg line_up"+lineNo, id:"line"+(lineNo)+'_up:'+stopNo+':['+names[lineNo][stopNo-1]+'-->'+names[lineNo][stopNo]+']'});
-                var newline_down = getNode('line', {x1: p1.x+dx, y1:p1.y-dy, x2:p2.x+dx, y2:p2.y-dy, 
-                                                    stroke:traffic_color_down, strokeWidth:sideWidth, strokeDasharray:dashLength+" "+dashLength, strokeDashoffset:dashLength*2,
-                                                    class:"tooltips-seg line_down"+lineNo, id:"line"+(lineNo)+'_down'+stopNo+':['+names[lineNo][stopNo]+'-->'+names[lineNo][stopNo-1]+']'});
-                var newline      = getNode('line', {x1: p1.x,    y1:p1.y,    x2:p2.x,    y2:p2.y, 
+                //console.log(lineNo+'_'+stopNo+':'+traffic_color_up+'/'+traffic_color_down+', '+index_up[lineNo][stopNo-1]+'/'+index_down[lineNo][stopNo-1]);
+                var newline_up   = getNode('line', {x1: p1.x-dx, y1:p1.y+dy+y_title, x2:p2.x-dx, y2:p2.y+dy+y_title, 
+                                                    stroke:traffic_color_up,   strokeWidth:sideWidth, strokeDashoffset:dashLength*2,
+                                                    class:"tooltips-seg line_up"+lineNo, 
+                                                    id:line_name[lineNo]+'路上行'+stopNo+'段：['+names[lineNo][stopNo-1]+'-->'+names[lineNo][stopNo]+']'});
+                var newline_down = getNode('line', {x1: p1.x+dx, y1:p1.y-dy+y_title, x2:p2.x+dx, y2:p2.y-dy+y_title, 
+                                                    stroke:traffic_color_down, strokeWidth:sideWidth, strokeDashoffset:dashLength*2,
+                                                    class:"tooltips-seg line_down"+lineNo, 
+                                                    id:line_name[lineNo]+'路下行'+stopNo+'段：['+names[lineNo][stopNo]+'-->'+names[lineNo][stopNo-1]+']'});
+                var newline      = getNode('line', {x1: p1.x,    y1:p1.y+y_title,    x2:p2.x,    y2:p2.y+y_title, 
                                                     stroke:'grey', strokeWidth:centerWidth, class:"tooltips-seg inner-seg line_inner"+lineNo, 
                                                     //id:"line"+(lineNo)+'_'+stopNo+':['+names[lineNo][stopNo-1]+','+names[lineNo][stopNo]+']'});
-                                                    id:"line"+(lineNo)+'_'+stopNo});
+                                                    id:line_name[lineNo]+'路_'+stopNo+'段'});
 
-                if (index_up[lineNo][stopNo-1]   > 0){ newline_up.classList.add("flow_up"); }
-                if (index_down[lineNo][stopNo-1] > 0){ newline_down.classList.add("flow_down"); }
+//                // comment-out if dash & line flow not wanted
+//                if (index_up[lineNo][stopNo-1]   > 0){ 
+//                    newline_up.setAttribute("stroke-dasharray",dashLength+" "+(50-dashLength)); 
+//                    newline_up.classList.add("flow_up"); 
+//                }     
+//                // comment-out if dash & line flow not wanted
+//                if (index_down[lineNo][stopNo-1] > 0){ 
+//                    newline_down.setAttribute("stroke-dasharray",dashLength+" "+(50-dashLength));                     
+//                    newline_down.classList.add("flow_down"); 
+//                }
 
                 svg.appendChild(newline_up);
                 svg.appendChild(newline_down);
@@ -171,13 +199,30 @@ function DC_render_map(n_line, n_stops, names, coordinates, index_up, index_down
             for (var stopNo=0; stopNo<n_stops[lineNo]; stopNo++){
                 p1.x = coordinates[lineNo][stopNo*2];
                 p1.y = coordinates[lineNo][stopNo*2+1];
-                var newdot = getNode('circle', {cx:p1.x, cy:p1.y, r:4, 
+                var newdot = getNode('circle', {cx:p1.x, cy:p1.y+y_title, r:dot_radius, 
                                     stroke:color_dot_stroke, fill:color_dot_fill, strokeWidth:dot_stoke_wid,
                                     class:"tooltips-stop line"+lineNo, 
-                                    id:"line"+(lineNo)+"_stop"+stopNo+':'+names[lineNo][stopNo]+' ('+p1.x+','+p1.y+')',//});
+                                    id:line_name[lineNo]+"路_"+(stopNo+1)+': '+names[lineNo][stopNo],//});
+                                    //id:"line"+(lineNo+1)+"_stop"+(stopNo+1)+':'+names[lineNo][stopNo]+' ('+p1.x+','+p1.y+')',//});
                                     onclick: "DC_addTable("+lineNo+", "+stopNo+")"}); // later: add id and class
                 svg.appendChild(newdot);
-                console.log('dot added: line '+ lineNo +':'+ (stopNo+1)+ ' at '+p1.x+','+p1.y);
+                
+
+                legend = getNode('text', {x: p1.x+10, y:p1.y+y_title, fontFamily:"Courier", fontSize:legendSize, fill:'#33CAFF'});
+    
+                // line break for long names
+                if (names[lineNo][stopNo].length > legendWid){
+                    legend.innerHTML = names[lineNo][stopNo].slice(0,legendWid)
+                                        +'<tspan x='+(p1.x+10)+' dy='+(legendSize*1.5)+'>'
+                                        +names[lineNo][stopNo].slice(legendWid)+'</tspan>';
+                    console.log('----line break: '+legend.innerHTML);
+                } else{
+                    legend.innerHTML = names[lineNo][stopNo];
+                }
+    
+                //legend.innerHTML = names[lineNo][stopNo];
+                svg.appendChild(legend);
+                //console.log('dot added: line '+ lineNo +':'+ (stopNo+1)+ ' at '+p1.x+','+p1.y);
                 
             }
         }
@@ -196,6 +241,7 @@ function DC_removeTable(){
 
 
 function DC_addTable(lineNo, stopNo) {
+
     // refresh the table area
     DC_removeTable();
     // refresh the inner segments
@@ -205,7 +251,7 @@ function DC_addTable(lineNo, stopNo) {
     }    
 
     $.getJSON(dataFile, function(data, status) {
-        console.log('read file for table');
+        //console.log('read file for table');
         //timeInput = 0
         data = data.map[0];
         DC_fillTable(data.n_line, data.n_stops, data.names,data.time_up[timeInput],data.time_down[timeInput], lineNo, stopNo);
@@ -234,12 +280,12 @@ function DC_fillTable(n_line, n_stops, names, time_up, time_down, lineNo, stopNo
     // highlight the chosen bus line
     var highlightSegs = document.getElementsByClassName("line_inner"+lineNo);//获取元素的class
     for (var i=0; i<highlightSegs.length; i++){
-        highlightSegs[i].style.stroke = 'yellow';
+        highlightSegs[i].style.stroke = 'white';//'yellow';
     }
 
     var tableDiv = document.getElementById('table_div');
     var title = document.createElement('h1');
-    title.innerHTML = '<br>公交'+(lineNo)+'路：由<span style="color:blue;font-size:110%;">'+names[lineNo][stopNo]+'</span>站出发时间';
+    title.innerHTML = '<br>公交'+(line_name[lineNo])+'路：由<span style="color:blue;font-size:110%;">'+names[lineNo][stopNo]+'</span>站出发时间';
     tableDiv.appendChild(title);
     var table = document.createElement('TABLE');
     var tableBody = document.createElement('TBODY');
@@ -263,7 +309,7 @@ function DC_fillTable(n_line, n_stops, names, time_up, time_down, lineNo, stopNo
     // assign rows of forwarding stops
     for (target = stopNo+1; target < n_stops[lineNo]; target += 1){    // i --> i+1,..., i --> n
         if (time_up[lineNo][target-1] < 0){
-            time += 1
+            time += 60
         } else {
             time += time_up[lineNo][target-1];
         }
@@ -273,10 +319,10 @@ function DC_fillTable(n_line, n_stops, names, time_up, time_down, lineNo, stopNo
     // assign rows of backwarding stops
     time = 0;
     for (target = stopNo-1; target >= 0; target -= 1){    // i --> i+1,..., i --> n
-        if (time_down[lineNo][target-1] < 0){
-            time += 1
+        if (time_down[lineNo][target] < 0){
+            time += 60
         } else {
-            time += time_down[lineNo][target-1];
+            time += time_down[lineNo][target];
         }
         timeTable[target] = new Array(target+1, names[lineNo][target], parseInt(time/60)+' 分 '+(time % 60)+' 秒', "下行");
     }
