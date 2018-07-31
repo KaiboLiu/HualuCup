aecf2be8179083cee4229f5d7260492cc4caf4fa
Liu Kaibo <liukaibo87@gmail.com>
2018-07-31 10:17:42 -0700

    remove upload script

--- upload_to_server.sh
+++
@@ -1,5 +0,0 @@
-scp ./demo.html liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/public_html/hualu
-scp -r ./js liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/public_html/hualu
-scp -r ./data liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/public_html/hualu
-scp -r ./css liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/public_html/hualu
-scp -r ./img liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/public_html/hualu
